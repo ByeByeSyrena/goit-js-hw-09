ccc90fa3d7ef183d2d7f3e2ec14c7eae978d3dc9
Ira S <iryna.shevchenko2@gmail.com>
2023-08-08 13:33:57 +0300

    npm install

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const refs = {
+    btnStart: document.querySelector('button[data-action="start"]'),
+    btnClose: document.querySelector('button[data-action="stop"]'),
+    body: document.querySelector("body"),
+};
+
+refs.btnStart.addEventListener("click", onStart);
+refs.btnClose.addEventListener("click", onClose);
+
+function onStart(event) {
+    const color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+    
+    timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+    }, 1000);
+    
+    if (event.target.quantity = 1) {
+        
+    }
+
+}
+
+function onClose(event) { 
+clearInterval(timerId);
+}
